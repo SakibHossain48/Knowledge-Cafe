e93ad4ec84d7982e00f36cf6d731d900b9ae74cd
Sakib Hossain <sakib199988@gmail.com>
2023-04-01 19:37:57 +0600

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <div className="header flex justify-between p-4 lg:px-12 lg:py-5 bg-slate-200">
+                <div className="navbar-brand">
+                    <h1 className='text-3xl font-semibold'>Knoewledge Cafe</h1>
+                </div>
+                <div className="user-profile w-10">
+                    <img className='rounded-full' src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/01/Mark_Zuckerberg_at_the_37th_G8_Summit_in_Deauville_018_square.jpg/800px-Mark_Zuckerberg_at_the_37th_G8_Summit_in_Deauville_018_square.jpg" alt="SHAKIB" />
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Header;
