2b37835195e78af6eedb0afe9312359e36847769
Sakib Hossain <sakib199988@gmail.com>
2023-04-01 22:42:41 +0600

    successfully done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
                     <h1 className='text-3xl font-semibold'>Knoewledge Cafe</h1>
                 </div>
                 <div className="user-profile w-10">
-                    <img className='rounded-full' src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/01/Mark_Zuckerberg_at_the_37th_G8_Summit_in_Deauville_018_square.jpg/800px-Mark_Zuckerberg_at_the_37th_G8_Summit_in_Deauville_018_square.jpg" alt="SHAKIB" />
+                    <img className='rounded-full' src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/01/Mark_Zuckerberg_at_the_37th_G8_Summit_in_Deauville_018_square.jpg/800px-Mark_Zuckerberg_at_the_37th_G8_Summit_in_Deauville_018_square.jpg" alt="MARK JUKERBERG" />
                 </div>
             </div>
         </div>
